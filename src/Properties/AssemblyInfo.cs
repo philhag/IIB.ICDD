3434b84f984fb0f5ebcdf5766eaa6b149f3315c0
Philipp Hagedorn <philipp.hagedorn@t-online.de>
2022-10-26 13:36:21 +0200

    Update AssemblyInfo.cs

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.9.7")]
 //[assembly: AssemblyVersion("1.0.0.49")]
 //[assembly: AssemblyFileVersion("1.0.0.49")]
 [assembly: NeutralResourcesLanguage("en-DE")]
